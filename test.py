2f4f9d0949ccec26bf9e12595e0f7ea5d2f3ea4b
bruce <vm.6vu65k3@gmail.com>
2025-12-22 09:33:35 +0800

    Modified test.py

--- test.py
+++ test.py
@@ -1,30 +1,9 @@
-from pprint import pprint
-from pydantic import ValidationError 
-from ..utils import ChartIn
-import json
+a = 2
+c = 3
 
-payload = {
-    "chart_type": "heatmap",
-    "params_json": {
-        "x_axis": "industry",
-        "y_axis": "age_level",
-        "value": "trans_total",
-        "value2": "trans_count"
-    },
-    "params_figure": {
-        "set_title": "所有產業各族群平均金額"
-    },
-    "create_by": 99116,
-    "filters": {
-        "start_month": "201401",
-        "end_month": "202111"
-    }
-}
+#加法器
+def add(number1, number2):
+    result = number1 + number2
+    return result
 
-pprint(payload)
-
-# try: 
-#     obj = ChartIn.model_validate(payload)
-#     print("OK", obj)
-# except ValidationError as e:
-#     print(e.json(indent = 2))
+print(add(a, c))
