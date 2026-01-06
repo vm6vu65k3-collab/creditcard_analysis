3b4d9a724f2510aa41dd36ba87d55e1108b28480
bruce <vm.6vu65k3@gmail.com>
2026-01-06 12:25:38 +0800

    test

--- creditcard_analysis/clean_data/testss.py
+++ creditcard_analysis/clean_data/testss.py
@@ -3,8 +3,11 @@ n1 = 3
 n2 = 7
 
 def add(n1: int, n2: int) -> int:
-    total = n1 + n2 
-    return (total)
+    a = n1 + n2 
+    return (a)
 
+def sub(n1: int, n2: int) -> int:
+    b = n1 + n2 
+    return (b)
 
 add(n1, n2)
